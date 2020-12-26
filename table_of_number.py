8610b88b35aa9d83e86484733a4298cb62b1f6f9
Danish <57083546+Danish-1@users.noreply.github.com>
2020-12-27 00:02:49 +0530

    Simple program

---
+++ table_of_number.py
@@ -0,0 +1,14 @@
+"""this program is written by danish.
+ at Fri 25 Dec 2020 , 12:10am. """
+ 
+print("enter a number to print its table")
+# use print function to teach users about this program.
+
+a=int(input("enter a number:"))
+# take an input and convert it to an integer value.
+
+for i in range(1,11): 
+                    # for loop run 1 to 10 or 10 times.
+	i = a*i     # a multiply by i which goes to 1 to10
+	print(i)   #then print i value.
+	
